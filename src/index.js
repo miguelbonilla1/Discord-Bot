8e900e80e4b3061d3a0de90a3509306c4aa77b83
miguel angel bonilla martinez <miguelbonilla1886@gmail.com>
2024-09-11 22:36:08 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { handleCryptoCommand } from './commands/crypto.js';
+import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -21,3 +22,34 @@ client.on('messageCreate', async (message) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+
+
+
+
+
+const app = express();
+
+
+
+
+
+// Ruta para el envío de correos
+app.post('/send-email', async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    await sendMail({ name, email, message });
+    res.status(200).send({ success: true });
+  } catch (error) {
+    res.status(500).send({ success: false, error: error.message });
+  }
+});
+
+// Para cualquier otra ruta, servir el archivo index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
